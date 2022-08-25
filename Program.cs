a589b0dc6a85144f84c4c0475ff1e42f6fa2acca
Dmitry <vip.mihaslw@mail>
2022-08-25 09:45:20 +0300

    task6

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 2 == 1)
+{
+Console.WriteLine("Число " + num1 + " является: НЕЧЁТНЫМ");
+}
+else
+{
+Console.WriteLine("Число " + num1 + "является: ЧЁТНЫМ");
+}
+break;
